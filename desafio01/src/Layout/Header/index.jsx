e0064a3265fe8a3a3d6451f19fb681e3dcbe6122
vctrhugoop <vhdestroy@hotmail.com>
2024-01-13 12:44:03 -0300

    ✨ feat: create header

--- desafio01/src/Layout/Header/index.jsx
+++ desafio01/src/Layout/Header/index.jsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <header className=" bg-zinc-900 px-6 py-12">
       <div className="mx-auto my-0 flex max-w-5xl flex-col items-center gap-y-8">
-        <h1 className="font-space-grotesk text-salmon text-2xl font-medium">
+        <h1 className="font-space-grotesk text-2xl font-medium text-salmon">
           Code<span className="text-zinc-100">lância</span>
         </h1>
         <Input />
