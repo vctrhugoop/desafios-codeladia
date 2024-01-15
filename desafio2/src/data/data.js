07b8556540ba19b4975b3927c8724ae821cf2d65
vctrhugoop <vhdestroy@hotmail.com>
2024-01-14 22:07:52 -0300

    fix: change import and path imagens products

--- desafio2/src/data/data.js
+++ desafio2/src/data/data.js
@@ -1,38 +1,45 @@
+import imgMidDutch from "../assets/air-jordan-1-mid-dutch-green-1-400.svg";
+import imgCMFT from "../assets/air-jordan-1-high-zoom-cmft-tropical-twist-1-400.svg";
+import imgCourt from "../assets/air-jordan-1-retro-high-court-purple-w-1-400.svg";
+import imgLightSmoke from "../air-jordan-1-mid-light-smoke-grey-gs-1-1000.svg";
+import imgBright from "../assets/air-jordan-1-mid-bright-citrus-1-1000 1.svg";
+import imgGrayCamo from "../assets/air-jordan-1-mid-grey-camo-1-1000 1.svg";
+
 export const products = [
   {
     name: "Air Jordan 1 Mid Dutch Green",
     category: "Tênis Air Jordan",
     price: "R$ 1.199,99",
-    url_img: "assets/air-jordan-1-mid-dutch-green-1-400.svg",
+    url_img: { imgMidDutch },
   },
   {
     name: "Air Jordan 1 High Zoom CMFT Tropical Twist",
     category: "Tênis Air Jordan",
     price: "R$ 1.049,00",
-    url_img: "assets/air-jordan-1-high-zoom-cmft-tropical-twist-1-400.svg",
+    url_img: { imgCMFT },
   },
   {
     name: "Air Jordan 1 High Court Purple",
     category: "Tênis Air Jordan",
     price: "R$ 1.199,99",
-    url_img: "assets/air-jordan-1-retro-high-court-purple-w-1-400.svg",
+    url_img: { imgCourt },
   },
   {
     name: "Air Jordan 1 Mid GS 'Light Smoke Grey'",
     category: "Tênis Air Jordan",
     price: "R$ 1.585,00",
-    url_img: "assets/air-jordan-1-mid-light-smoke-grey-gs-1-1000.svg",
+    url_img: { imgLightSmoke },
   },
   {
     name: "Air Jordan 1 Mid SE Bright Citrus",
     category: "Tênis Air Jordan",
     price: "R$ 1.199,99",
-    url_img: "assets/air-jordan-1-mid-bright-citrus-1-1000 1.svg",
+    url_img: { imgBright },
   },
   {
-    name: "Air Jordan 1 Mid Grey Camo",
+    name: "Air Jordan 1 Mid Gray Camo",
     category: "Tênis Air Jordan",
     price: "R$ 999,99",
-    url_img: "assets/air-jordan-1-mid-grey-camo-1-1000 1.svg",
+    url_img: { imgGrayCamo },
   },
 ];
